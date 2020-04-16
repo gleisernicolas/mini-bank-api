3a1adfea62d523fa0e8b899d79ccf5bf09cbccf8
Nicolas Gleiser <ncs.cr4sh@gmail.com>
2020-04-16 04:39:55 -0300

    add first real spec for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new }
-
   context 'validations' do
-    it { expect(user).to validate_uniqueness_of(:cpf) }
+    it { should validate_uniqueness_of(:cpf).case_insensitive }
   end
 end
